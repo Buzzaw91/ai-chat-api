c0edd0521bf373d1fbf124f65777b449b6973d3c
Aleksi Ylönen <aleksi.ylonen2@gmailcom>
2023-12-13 19:03:41 +0200

    app setup, docker-compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'mariadb',
+        host: process.env.MYSQL_HOST,
+        port: 3306,
+        username: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PWD,
+        database: process.env.MYSQL_DATABASE,
+        entities: [__dirname + '/**/**/**.entity{.ts,.js}'],
+        synchronize: true,
+        migrations: [__dirname + '/migrations/*{.ts,.js}'],
+        cli: {
+          migrationsDir: __dirname + '/migrations',
+        },
+        charset: 'utf8mb4',
+        migrationsRun: true,
+      }),
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import * as dotenv from 'dotenv';
+import * as cookieParser from 'cookie-parser';
+
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: true,
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+      credentials: true,
+    },
+  });
+
+  app.use(cookieParser());
+  app.use(helmet());
+
   await app.listen(3000);
 }
 bootstrap();
