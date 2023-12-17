60808e13a2f316802163f17ec995a138071c7000
Aleksi Ylönen <aleksi.ylonen2@gmailcom>
2023-12-17 20:32:47 +0200

    re-enable devtoosl settings

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule, {
     httpsOptions,
+    snapshot: true,
     cors: {
       origin: true,
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
