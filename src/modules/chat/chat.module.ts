0d929d42017d3eca7ee27c8e88095be02f7a8882
Aleksi Ylönen <aleksi.ylonen2@gmailcom>
2023-12-22 15:40:39 +0200

    vertexAI instance creation

--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -1,10 +1,38 @@
 import { Module } from '@nestjs/common'
+import {
+  VertexAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from '@google-cloud/vertexai'
 import { ChatController } from './chat.controller'
 import { ChatService } from './chat.service'
 
 @Module({
   controllers: [ChatController],
-  providers: [ChatService],
+  providers: [
+    {
+      provide: 'GENERATIVE_MODEL',
+      useFactory: () => {
+        const project = process.env.GCP_PROJECT
+        const location = process.env.GCP_LOCATION
+        const vertexAI = new VertexAI({
+          project,
+          location,
+        })
+        return vertexAI.preview.getGenerativeModel({
+          model: 'gemini-pro',
+          safety_settings: [
+            {
+              category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+              threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+            },
+          ],
+          generation_config: { max_output_tokens: 256 },
+        })
+      },
+    },
+    ChatService,
+  ],
   exports: [],
 })
 export class ChatModule {}
